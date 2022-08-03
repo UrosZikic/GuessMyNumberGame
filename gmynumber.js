56cc52fa4af51929200ec128407c027156e18927
Uros <uroszikic1512988@gmail.com>
2022-08-03 16:21:42 +0200

    Update gmynumber.js

--- gmynumber.js
+++ gmynumber.js
@@ -49,4 +49,15 @@ function resetFunction() {
   document.querySelector('#score').textContent = '20';
 }
 
+// this code allows the user to input a number via 'enter' key
+input.addEventListener('keypress', function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === 'Enter') {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById('check-btn').click();
+  }
+});
+
 
